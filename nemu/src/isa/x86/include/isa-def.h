b88b1e9806fcb1da2fef327332388ec779d55cbc
tracer-ics2024 <tracer@njuics.org>
2024-11-06 22:25:59 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.3-2-default #1 SMP PREEMPT_DYNAMIC Fri Oct 11 06:48:36 UTC 2024 (7881e90) x86_64 x86_64 x86_64 GNU/Linux  22:25:59  up 11 days  3:58,  5 users,  load average: 0.26, 0.85, 0.90

--- nemu/src/isa/x86/include/isa-def.h
+++ nemu/src/isa/x86/include/isa-def.h
@@ -35,10 +35,10 @@ typedef union {
 
   /* Do NOT change the order of the GPRs' definitions. */
   struct {
-    uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi, pc;
+    uint32_t eax, ecx, edx, ebx, esp, ebp, esi, edi;
+    vaddr_t pc;
   };
 
-  // vaddr_t pc;
 } x86_CPU_state;
 
 // decode
