6550894b625e6cbdc552f8288682c3c98f0b75ed
tracer-ics2024 <tracer@njuics.org>
2024-11-07 12:14:20 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.3-2-default #1 SMP PREEMPT_DYNAMIC Fri Oct 11 06:48:36 UTC 2024 (7881e90) x86_64 x86_64 x86_64 GNU/Linux  12:14:20  up 11 days 17:46,  4 users,  load average: 0.64, 0.78, 0.77

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -122,12 +122,10 @@ static int cmd_x(char *args) {
 // - r : registers
 // - w : watchpoints
 static int cmd_info(char *args) {
-  char *arg = args;
   int argc = 0;
-  arg = strtok(arg, " ");
+  char *arg = strtok(args, " ");
   while (arg != NULL) {
     argc++;
-    printf("arg: %s\n", arg);
     arg = strtok(NULL, " ");
   }
   if (argc != 1) {
@@ -140,17 +138,15 @@ static int cmd_info(char *args) {
 
 static int cmd_help(char *args) {
   /* extract the first argument */
-  char *arg = strtok(NULL, " ");
-  printf("arg: %s\n", arg);
-  int i;
+  char *arg = strtok(args, " ");
 
   if (arg == NULL) {
     /* no argument given */
-    for (i = 0; i < NR_CMD; i++) {
+    for (int i = 0; i < NR_CMD; i++) {
       printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
     }
   } else {
-    for (i = 0; i < NR_CMD; i++) {
+    for (int i = 0; i < NR_CMD; i++) {
       if (strcmp(arg, cmd_table[i].name) == 0) {
         printf("%s - %s\n", cmd_table[i].name, cmd_table[i].description);
         return 0;
