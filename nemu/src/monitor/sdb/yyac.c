17cda0f4830e31b834003e4c2c23cb1d7c261897
tracer-ics2024 <tracer@njuics.org>
2024-11-08 17:55:17 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.3-2-default #1 SMP PREEMPT_DYNAMIC Fri Oct 11 06:48:36 UTC 2024 (7881e90) x86_64 x86_64 x86_64 GNU/Linux  17:55:17  up 12 days 23:27,  4 users,  load average: 0.44, 0.38, 0.55

--- nemu/src/monitor/sdb/yyac.c
+++ nemu/src/monitor/sdb/yyac.c
@@ -1,4 +1,5 @@
 #include "expr.h"
+#include "expr.tab.h"
 
 bool yy_success = true;
 word_t yy_result = 0;
@@ -8,10 +9,26 @@ int current_token = 0;
 int yylex(void) {
   Token tok = tokens[current_token];
   current_token++;
-  if (tok.type == TK_NUM) {
-    return tok.val;
+  switch (tok.type) {
+    case TK_NUM:
+      return TK_NUM_;
+
+    case TK_EQ:
+      return TK_EQ_;
+    case TK_NE:
+      return TK_NE_;
+    case TK_GE:
+      return TK_GE_;
+    case TK_LE:
+      return TK_LE_;
+    case TK_GT:
+      return TK_GT_;
+    case TK_LT:
+      return TK_LT_;
+
+    default:  // + - * / ( )
+      return tok.type;
   }
-  return tok.type;
 }
 
 void yyerror(const char* s) {
