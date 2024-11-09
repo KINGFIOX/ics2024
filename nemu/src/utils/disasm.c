56aa6c2a9984e9bc0b2cd5a8abe7832fc5d73a93
tracer-ics2024 <tracer@njuics.org>
2024-11-09 22:40:46 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.3-2-default #1 SMP PREEMPT_DYNAMIC Fri Oct 11 06:48:36 UTC 2024 (7881e90) x86_64 x86_64 x86_64 GNU/Linux  22:40:46  up 14 days  4:13,  4 users,  load average: 0.18, 0.34, 0.62

--- nemu/src/utils/disasm.c
+++ nemu/src/utils/disasm.c
@@ -16,6 +16,7 @@
 #include <capstone/capstone.h>
 #include <common.h>
 #include <dlfcn.h>
+#include <stdio.h>
 
 static size_t (*cs_disasm_dl)(csh handle, const uint8_t *code, size_t code_size, uint64_t address, size_t count, cs_insn **insn);
 static void (*cs_free_dl)(cs_insn *insn, size_t count);
@@ -63,7 +64,7 @@ void init_disasm() {
 void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte) {
   cs_insn *insn;
   size_t count = cs_disasm_dl(handle, code, nbyte, pc, 0, &insn);
-  assert(count == 1);
+  // assert(count == 1);
   int ret = snprintf(str, size, "%s", insn->mnemonic);
   if (insn->op_str[0] != '\0') {
     snprintf(str + ret, size - ret, "\t%s", insn->op_str);
