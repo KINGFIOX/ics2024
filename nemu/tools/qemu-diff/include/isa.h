2c101ef384790e683ac3beaa86ff2de10c4c40ea
tracer-ics2024 <tracer@njuics.org>
2024-11-10 21:16:50 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  21:16:50  up   5:39,  3 users,  load average: 0.66, 0.91, 0.68

--- nemu/tools/qemu-diff/include/isa.h
+++ nemu/tools/qemu-diff/include/isa.h
@@ -1,37 +1,36 @@
 /***************************************************************************************
-* Copyright (c) 2014-2024 Zihao Yu, Nanjing University
-*
-* NEMU is licensed under Mulan PSL v2.
-* You can use this software according to the terms and conditions of the Mulan PSL v2.
-* You may obtain a copy of Mulan PSL v2 at:
-*          http://license.coscl.org.cn/MulanPSL2
-*
-* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
-* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
-* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
-*
-* See the Mulan PSL v2 for more details.
-***************************************************************************************/
+ * Copyright (c) 2014-2024 Zihao Yu, Nanjing University
+ *
+ * NEMU is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
 
 #ifndef __ISA_H__
 #define __ISA_H__
 
 #if defined(CONFIG_ISA_mips32)
 #define ISA_QEMU_BIN "qemu-system-mipsel"
-#define ISA_QEMU_ARGS "-machine", "mipssim",\
-  "-kernel", NEMU_HOME "/resource/mips-elf/mips.dummy",
+#define ISA_QEMU_ARGS "-machine", "mipssim", "-kernel", NEMU_HOME "/resource/mips-elf/mips.dummy",
 #elif defined(CONFIG_ISA_riscv) && !defined(CONFIG_RV64)
 #define ISA_QEMU_BIN "qemu-system-riscv32"
 #define ISA_QEMU_ARGS "-bios", "none",
 #elif defined(CONFIG_ISA_riscv) && defined(CONFIG_RV64)
 #define ISA_QEMU_BIN "qemu-system-riscv64"
-#define ISA_QEMU_ARGS 
+#define ISA_QEMU_ARGS
 #elif defined(CONFIG_ISA_x86)
 #define ISA_QEMU_BIN "qemu-system-i386"
 #define ISA_QEMU_ARGS
 #elif defined(CONFIG_ISA_loongarch32r)
 #define ISA_QEMU_BIN "qemu-system-loongarch32"
-#define ISA_QEMU_ARGS "-M","ls3a5k32",
+#define ISA_QEMU_ARGS "-M", "ls3a5k32",
 #else
 #error Unsupport ISA
 #endif
@@ -44,7 +43,7 @@ union isa_gdb_regs {
 #elif defined(CONFIG_ISA_riscv) && !defined(CONFIG_RV64)
     uint32_t gpr[32];
     uint32_t pc;
-#elif defined(CONFIG_ISA_riscv) &&  defined(CONFIG_RV64)
+#elif defined(CONFIG_ISA_riscv) && defined(CONFIG_RV64)
     uint64_t gpr[32];
     uint64_t fpr[32];
     uint64_t pc;
@@ -53,8 +52,8 @@ union isa_gdb_regs {
     uint32_t eip, eflags;
     uint32_t cs, ss, ds, es, fs, gs;
 #elif defined(CONFIG_ISA_loongarch32r)
-  uint32_t gpr[32];
-  uint32_t pc;
+    uint32_t gpr[32];
+    uint32_t pc;
 #endif
   };
   struct {
