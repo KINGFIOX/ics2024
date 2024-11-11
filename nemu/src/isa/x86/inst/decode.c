9e1d52904d53c4f48ab5d77aebf03ed09a6bbca4
tracer-ics2024 <tracer@njuics.org>
2024-11-11 14:08:11 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  14:08:11  up  22:31,  3 users,  load average: 1.36, 0.61, 0.31

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -358,6 +358,7 @@ static void decode_operand(Decode *s, uint8_t opcode, int *rd_, word_t *src1, wo
       break;
     case TYPE_SI2E:
       decode_rm(s, rd_, addr, gp_idx, w);
+      printf("rd_ = %s, addr = 0x%08x, gp_idx = %s\n", reg_name(*rd_, 4), *addr, reg_name(*gp_idx, 4));
       imm();
       break;
     case TYPE_r:
