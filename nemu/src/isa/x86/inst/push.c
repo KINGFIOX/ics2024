5017f492c3c54a7c6c926e8628ed6fd2ece9d2bc
tracer-ics2024 <tracer@njuics.org>
2024-11-11 23:28:28 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  23:28:28  up 1 day  7:51,  5 users,  load average: 0.03, 0.15, 0.17

--- nemu/src/isa/x86/inst/push.c
+++ nemu/src/isa/x86/inst/push.c
@@ -9,21 +9,21 @@ void push(int width, word_t data) {
   printf("width = %d, data = %x\n", width, data);
   assert(width == 1 || width == 2 || width == 4);
 
-  vaddr_t vaddr = reg_read(R_ESP, width);  // get %esp, %esp is 4 bytes
+  vaddr_t vaddr = reg_read(R_ESP, 4);  // get %esp, %esp is 4 bytes
   vaddr -= width;
-  reg_write(R_ESP, width, vaddr);
+  reg_write(R_ESP, 4, vaddr);
   vaddr_write(vaddr, width, data);
 
-  Assert(vaddr_read(reg_read(R_ESP, width), width) == data, "push error");
+  Assert(vaddr_read(reg_read(R_ESP, 4), width) == data, "push error");
 }
 
 word_t pop(int width) {
   assert(width == 1 || width == 2 || width == 4);
 
-  vaddr_t vaddr = reg_read(R_ESP, width);
+  vaddr_t vaddr = reg_read(R_ESP, 4);
   word_t data = vaddr_read(vaddr, width);
   vaddr += width;
-  reg_write(R_ESP, width, vaddr);
+  reg_write(R_ESP, 4, vaddr);
   return data;
 }
 
