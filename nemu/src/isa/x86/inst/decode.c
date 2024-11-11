e83fe7b754d52fb7c7071cf40160dbda71e7a11d
tracer-ics2024 <tracer@njuics.org>
2024-11-11 18:42:11 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  18:42:11  up 1 day  3:05,  5 users,  load average: 0.47, 0.34, 0.32

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -517,11 +517,11 @@ again:
     word_t op1 = Rr(rd, w);
     word_t op2 = vaddr_read(addr, w);
     cpu.eflags.of = 0;
-    cpu.eflags.sf = op1 < op2;
-    cpu.eflags.zf = op1 == op2;
+    cpu.eflags.sf = 0;
+    cpu.eflags.zf = 0;
     cpu.eflags.af = 0;
     cpu.eflags.pf = 0;
-    cpu.eflags.cf = 0;
+    cpu.eflags.cf = op1 < op2;
   });
 
   //   10002f:       ff 71 fc                push   -0x4(%ecx)
