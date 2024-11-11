27ea5ce66bb35d7a2c6cab7b56ea9dc8cfabee29
tracer-ics2024 <tracer@njuics.org>
2024-11-11 13:01:08 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  13:01:08  up  21:24,  3 users,  load average: 0.24, 0.14, 0.04

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -214,6 +214,8 @@ static void push(int width, word_t data) {
   assert(width == 1 || width == 2 || width == 4);
 
   vaddr_t vaddr = reg_read(R_ESP, 4);
+  reg_write(R_ESP, 4, vaddr - width);
+
   vaddr_write(vaddr, width, data);
 }
 
