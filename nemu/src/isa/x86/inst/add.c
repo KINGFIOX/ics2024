bf2e93ae951d62999cae92da6936ae51b1afb12c
tracer-ics2024 <tracer@njuics.org>
2024-11-11 23:51:48 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  23:51:48  up 1 day  8:14,  4 users,  load average: 0.28, 0.25, 0.19

--- nemu/src/isa/x86/inst/add.c
+++ nemu/src/isa/x86/inst/add.c
@@ -1,3 +1,9 @@
+#include <stdio.h>
+
 #include "inst.h"
 
-void add(int rd, int w, int rs) { reg_write(rd, w, Rr(rd, w) + Rr(rs, w)); }
+void add(int rd, int w, int rs) {
+  printf("rd = %d, w = %d, rs = %d\n", rd, w, rs);
+  printf("Rr(rd, w) = %x, Rr(rs, w) = %x\n", Rr(rd, w), Rr(rs, w));
+  reg_write(rd, w, Rr(rd, w) + Rr(rs, w));
+}
