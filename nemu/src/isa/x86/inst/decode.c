876318aad28b9fa9405e3d7d652c0e958840bd93
tracer-ics2024 <tracer@njuics.org>
2024-11-12 17:00:33 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  17:00:33  up 2 days  1:23,  4 users,  load average: 0.08, 0.21, 0.26

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -494,7 +494,7 @@ static inline void div1(int w, word_t divisor) {
         if (rs == -1) {                                                \
           div1(w, Mr(addr, w));                                        \
         } else {                                                       \
-          div1(w, Rr(rs, w));                                          \
+          div1(w, Rr(rd, w));                                          \
         }                                                              \
         break;                                                         \
       case 0b111: /*idiv*/                                             \
