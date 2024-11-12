7e033dc69de77fddc382c126820710b7fa3ec7e9
tracer-ics2024 <tracer@njuics.org>
2024-11-12 13:20:17 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  13:20:17  up 1 day 21:43,  4 users,  load average: 0.07, 0.22, 0.25

--- nemu/src/isa/x86/inst/arithmetic.c
+++ nemu/src/isa/x86/inst/arithmetic.c
@@ -100,8 +100,6 @@ word_t xor_(int w, word_t op1, word_t op2) {
 word_t not_(int w, word_t op1) { return xor_(w, op1, all); }
 
 word_t shr(int w, word_t op1, word_t op2) {
-  printf("shr 0x%x 0x%x\n", op1, op2);
-
   word_t ret = op1 >> op2;
 
   // TODO: sf, cf, of. 可能有循环移位之类的, 没法确定
@@ -111,8 +109,6 @@ word_t shr(int w, word_t op1, word_t op2) {
 
   // pf
   cpu.eflags.pf = (1 == ones(ret) % 2);
-
-  printf("shr ret 0x%x\n", ret);
 
   return ret;
 }
