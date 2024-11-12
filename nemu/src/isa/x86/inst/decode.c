c41b77acb27605b6b71a6f80a45c54c37df06d29
tracer-ics2024 <tracer@njuics.org>
2024-11-12 23:45:25 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  23:45:25  up 2 days  8:08,  4 users,  load average: 0.75, 0.47, 0.40

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -615,7 +615,7 @@ void _2byte_esc(Decode *s, bool is_operand_size_16) {
   //   10006a:       0f b6 d2                movzbl %dl,%edx
   INSTPAT("1011 0110", movzbl, Eb2G, 0, assert(w == 4); Rw(rd, 4, Rr(rs, 1)));
   //   100043:       0f bf 84 1b 40 02 10    movswl 0x100240(%ebx,%ebx,1),%eax
-  INSTPAT("1011 1111", movswl, Ew2G, 4, assert(w == 4); Rw(rs, 4, SEXT(Mr(addr, 2), 4 * 8)));
+  INSTPAT("1011 1111", movswl, Ew2G, 4, assert(w == 4); Rw(rs, 4, SEXT(Mr(addr, 2), 2 * 8)));
   //   100160:       0f 85 8e 01 00 00       jne    1002f4 <__udivmoddi4+0x1c0>
   INSTPAT("1000 ????", jne, J, 4, jcc());
   INSTPAT("???? ????", inv, N, 0, INV(s->pc));
