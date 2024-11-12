6147f03964cbf371074e1ea290d88986c1fcc6b5
tracer-ics2024 <tracer@njuics.org>
2024-11-12 22:10:22 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  22:10:22  up 2 days  6:33,  4 users,  load average: 0.24, 0.31, 0.27

--- nemu/src/isa/x86/inst/arithmetic.c
+++ nemu/src/isa/x86/inst/arithmetic.c
@@ -28,7 +28,7 @@ word_t add(int w, word_t op1_, word_t op2_) {
 
   uint64_t ret_u64 = op1 + op2;
   const int sign_mask = (1 << 31);
-  const uint64_t mask = (uint32_t)~0;
+  const uint64_t mask = (uint32_t)((1 << (w * 8)) - 1);
 
   cpu.eflags.cf = !!(ret_u64 & mask);                                                                      // cf
   cpu.eflags.pf = (1 == ones(ret_u64 & mask) % 2);                                                         // pf
