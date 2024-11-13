b60e256afa81c0e2b856e290c8d91507cd5d5935
tracer-ics2024 <tracer@njuics.org>
2024-11-13 21:32:30 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  21:32:30  up 3 days  5:55,  6 users,  load average: 0.12, 0.48, 0.53

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -27,16 +27,89 @@ int atoi(const char *nptr) {
   return x;
 }
 
-void *malloc(size_t size) {
-  // On native, malloc() will be called during initializaion of C runtime.
-  // Therefore do not call panic() here, else it will yield a dead recursion:
-  //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-  panic("Not implemented");
-#endif
-  return NULL;
+extern Area heap;
+
+typedef long Align;
+
+union header {
+  struct {
+    union header *ptr;
+    uint32_t size;
+  } s;
+  Align x;
+};
+
+typedef union header Header;
+
+static Header base;
+static Header *freep;
+
+void free(void *ap) {
+  Header *p;
+  Header *bp = (Header *)ap - 1;
+  for (p = freep; !(bp > p && bp < p->s.ptr); p = p->s.ptr)
+    if (p >= p->s.ptr && (bp > p || bp < p->s.ptr)) break;
+  if (bp + bp->s.size == p->s.ptr) {
+    bp->s.size += p->s.ptr->s.size;
+    bp->s.ptr = p->s.ptr->s.ptr;
+  } else
+    bp->s.ptr = p->s.ptr;
+  if (p + p->s.size == bp) {
+    p->s.size += bp->s.size;
+    p->s.ptr = bp->s.ptr;
+  } else {
+    p->s.ptr = bp;
+  }
+  freep = p;
 }
 
-void free(void *ptr) {}
+// On native, malloc() will be called during initializaion of C runtime.
+// Therefore do not call panic() here, else it will yield a dead recursion:
+//   panic() -> putchar() -> (glibc) -> malloc() -> panic()
+
+#if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
+
+static Header *morecore(uint32_t nu) {
+  if (nu < 4096) nu = 4096;
+  char *p = (char *)heap.start;
+  if (p == (char *)-1) return 0;
+  Header *hp = (Header *)p;
+  hp->s.size = nu;
+  free((void *)(hp + 1));
+  return freep;
+}
+
+void *malloc(size_t nbytes) {
+  Header *prevp;
+  const uint32_t nunits = (nbytes + sizeof(Header) - 1) / sizeof(Header) + 1;
+  if ((prevp = freep) == 0) {
+    base.s.ptr = freep = prevp = &base;
+    base.s.size = 0;
+  }
+  for (Header *p = prevp->s.ptr;; prevp = p, p = p->s.ptr) {
+    if (p->s.size >= nunits) {
+      if (p->s.size == nunits) {
+        prevp->s.ptr = p->s.ptr;
+      } else {
+        p->s.size -= nunits;
+        p += p->s.size;
+        p->s.size = nunits;
+      }
+      freep = prevp;
+      return (void *)(p + 1);
+    }
+    if (p == freep) {
+      if ((p = morecore(nunits)) == 0) {
+        return 0;
+      }
+    }
+  }
+}
+
+#else
+
+void *malloc(size_t size) { return NULL; }
+
+#endif
 
 #endif
