f19a0478e2c9398a508bc74ec1f2e89da0e18e67
tracer-ics2024 <tracer@njuics.org>
2024-11-13 14:53:24 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  14:53:24  up 2 days 23:16,  6 users,  load average: 1.02, 0.62, 0.46

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,28 +1,115 @@
 #include <am.h>
-#include <klib.h>
 #include <klib-macros.h>
+#include <klib.h>
 #include <stdarg.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int printf(const char *fmt, ...) {
-  panic("Not implemented");
+static int sputc(char *s, char c) {
+  *s = c;
+  return 1;
 }
 
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  panic("Not implemented");
+static const char digits[] = "0123456789abcdef";
+
+static int sprintint(char *s, int xx, int base, int sign) {
+  char buf[16];
+  int i, n;
+  uint32_t x;
+
+  if (sign && (sign = xx < 0))
+    x = -xx;
+  else
+    x = xx;
+
+  i = 0;
+  do {
+    buf[i++] = digits[x % base];
+  } while ((x /= base) != 0);
+
+  if (sign) buf[i++] = '-';
+
+  n = 0;
+  while (--i >= 0) n += sputc(s + n, buf[i]);
+  return n;
 }
+
+int printf(const char *fmt, ...) {
+  if (fmt == 0) panic("null fmt");
+
+  va_list ap;
+  va_start(ap, fmt);
+
+  panic("Not implemented");
+
+  return -1;
+}
+
+int vsprintf(char *buf, const char *fmt, va_list ap) { return vsnprintf(buf, SIZE_MAX, fmt, ap); }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  if (fmt == 0) panic("null fmt");
+
+  va_list ap;
+  va_start(ap, fmt);
+
+  int off = vsnprintf(out, SIZE_MAX, fmt, ap);
+
+  va_end(ap);
+
+  return off;
 }
 
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-  panic("Not implemented");
+int snprintf(char *buf, size_t sz, const char *fmt, ...) {
+  if (fmt == 0) panic("null fmt");
+
+  va_list ap;
+  va_start(ap, fmt);
+
+  int off = vsnprintf(buf, sz, fmt, ap);
+
+  va_end(ap);
+
+  return off;
 }
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-  panic("Not implemented");
+int vsnprintf(char *buf, size_t sz, const char *fmt, va_list ap) {
+  if (fmt == 0) panic("null fmt");
+
+  int c;
+  char *s;
+  int off = 0;
+
+  for (int i = 0; off < sz && (c = fmt[i] & 0xff) != 0; i++) {
+    if (c != '%') {
+      off += sputc(buf + off, c);
+      continue;
+    }
+    c = fmt[++i] & 0xff;
+    if (c == 0) break;
+    switch (c) {
+      case 'd':
+        off += sprintint(buf + off, va_arg(ap, int), 10, 1);
+        break;
+      case 'x':
+        off += sprintint(buf + off, va_arg(ap, int), 16, 1);
+        break;
+      case 's':
+        if ((s = va_arg(ap, char *)) == 0) s = "(null)";
+        for (; *s && off < sz; s++) off += sputc(buf + off, *s);
+        break;
+      case '%':
+        off += sputc(buf + off, '%');
+        break;
+      default:
+        // Print unknown % sequence to draw attention.
+        off += sputc(buf + off, '%');
+        off += sputc(buf + off, c);
+        break;
+    }
+  }
+
+  return off;
 }
 
 #endif
--- abstract-machine/scripts/x86-nemu.mk
+++ abstract-machine/scripts/x86-nemu.mk
@@ -1,6 +1,6 @@
 include $(AM_HOME)/scripts/isa/x86.mk
 include $(AM_HOME)/scripts/platform/nemu.mk
-CFLAGS  += -mstringop-strategy=loop -DISA_H=\"x86/x86.h\"
+CFLAGS  += -DISA_H=\"x86/x86.h\" # -mstringop-strategy=loop
 # overwrite _pmem_start and _entry_offset defined in nemu.mk
 LDFLAGS += --defsym=_pmem_start=0x0 --defsym=_entry_offset=0x100000
 
