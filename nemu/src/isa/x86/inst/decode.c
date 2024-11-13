9ea2edc58a90d219ff616729c82133c3f3494006
tracer-ics2024 <tracer@njuics.org>
2024-11-13 21:36:46 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  21:36:46  up 3 days  5:59,  6 users,  load average: 0.43, 0.35, 0.45

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -746,6 +746,8 @@ again:
   INSTPAT("0000 1010", or, E2G, 1, Rw(rd, w, or_(w, Rr(rd, w), Mr(addr, w))));
   //   100071:       22 02                   and    (%edx),%al
   INSTPAT("0010 0010", and, E2G, 1, Rw(rd, w, and_(w, Rr(rd, w), Mr(addr, w))));
+  //   1045f0:       21 d0                   and    %edx,%eax
+  INSTPAT("0010 0001", and, G2E, 0, Rw(rd, w, and_(w, Rr(rd, w), Rr(rs, w))));
   //   10004d:       32 06                   xor    (%esi),%al
   INSTPAT("0011 0010", xor, E2G, 1, Rw(rd, w, xor_(w, Rr(rd, w), Mr(addr, w))));
   //   100052:       33 14 85 e0 01 10 00    xor    0x1001e0(,%eax,4),%edx
