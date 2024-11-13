1413ef909b9749b25f47980fa3690c66c630e247
tracer-ics2024 <tracer@njuics.org>
2024-11-13 21:15:19 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  21:15:19  up 3 days  5:38,  6 users,  load average: 0.57, 0.43, 0.46

--- abstract-machine/am/src/platform/nemu/trm.c
+++ abstract-machine/am/src/platform/nemu/trm.c
@@ -5,11 +5,9 @@ extern char _heap_start;
 int main(const char *args);
 
 Area heap = RANGE(&_heap_start, PMEM_END);
-static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER; // defined in CFLAGS
+static const char mainargs[MAINARGS_MAX_LEN] = MAINARGS_PLACEHOLDER;  // defined in CFLAGS
 
-void putch(char ch) {
-  outb(SERIAL_PORT, ch);
-}
+void putch(char ch) { outb(SERIAL_PORT, ch); }
 
 void halt(int code) {
   nemu_trap(code);
