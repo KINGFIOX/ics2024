8dd2fa6588e3e7a8304bf45c15eb0853d56fb781
tracer-ics2024 <tracer@njuics.org>
2024-11-13 17:08:01 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  17:08:01  up 3 days  1:31,  6 users,  load average: 0.37, 0.19, 0.19

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -7,14 +7,14 @@
 
 #define BUF_SIZE (4096)
 
-int sputc(char *s, char c) {
+static inline int sputc(char *s, char c) {
   *s = c;
   return 1;
 }
 
 static const char digits[] = "0123456789abcdef";
 
-int sprintint(char *s, int xx, int base, int sign) {
+static inline int sprintint(char *s, int xx, int base, int sign) {
   char buf[16];
   memset(buf, 0, 16);
 
