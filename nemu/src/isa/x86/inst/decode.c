c56813c0a5a5997f59e7cf985329c93cd671aa5e
tracer-ics2024 <tracer@njuics.org>
2024-11-13 17:23:07 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.5-1-default #1 SMP PREEMPT_DYNAMIC Wed Oct 23 04:27:11 UTC 2024 (b4e3aa9) x86_64 x86_64 x86_64 GNU/Linux  17:23:07  up 3 days  1:46,  6 users,  load average: 0.49, 0.28, 0.18

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -706,6 +706,14 @@ static inline void test_rm(int w, int rd, int rs, vaddr_t addr) {
   }
 }
 
+static inline void add_rm(int w, int rd, int rs, vaddr_t addr) {
+  if (rd == -1) {
+    Mw(addr, w, add(w, Mr(addr, w), Rr(rs, w), false));
+  } else {
+    Rw(rd, w, add(w, Rr(rd, w), Rr(rs, w), false));
+  }
+}
+
 int isa_exec_once(Decode *s) {
   bool is_operand_size_16 = false;
   uint8_t opcode = 0;
@@ -844,7 +852,7 @@ again:
   INSTPAT("0100 1???", dec, r, 0, Rw(rd, w, sub(w, Rr(rd, w), 1, false)));
 
   // 10005e:       01 f2                   add    %esi,%edx
-  INSTPAT("0000 0001", add, G2E, 0, assert(rs != -1); assert(rd != -1); Rw(rd, w, add(w, Rr(rd, w), Rr(rs, w), false)));
+  INSTPAT("0000 0001", add, G2E, 0, add_rm(w, rd, rs, addr));
 
   //   100043:       03 04 9d dc 01 10 00    add    0x1001dc(,%ebx,4),%eax
   INSTPAT("0000 0011", add, E2G, 0, Rw(rd, w, add(w, Rr(rd, w), Mr(addr, w), false)));
