a4f3b56a8d7f2098d462003fe82e2af108fb6f93
tracer-ics2024 <tracer@njuics.org>
2024-11-15 10:53:44 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.6-2-default #1 SMP PREEMPT_DYNAMIC Mon Nov  4 08:15:48 UTC 2024 (8545f24) x86_64 x86_64 x86_64 GNU/Linux  10:53:44  up  23:51,  7 users,  load average: 0.48, 0.48, 0.45

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -702,11 +702,15 @@ static inline word_t in_(int w, word_t port) {
 
 static inline void rep(Decode *s, int w) {
   uint8_t opcode = x86_inst_fetch(s, 1);
+  int rs = opcode & 0b0111;
   switch (opcode) {
     case 0xa5:
       Mw(Rr(R_EDI, w), w, Mr(Rr(R_ESI, w), w));
-      Rw(R_ECX, w, Rr(R_ECX, w) - 1);
       Rw(R_ESI, w, Rr(R_ESI, w) + w);
+      Rw(R_EDI, w, Rr(R_EDI, w) + w);
+      break;
+    case 0xab:
+      Mw(Rr(R_EDI, w), w, Rr(rs, w));
       Rw(R_EDI, w, Rr(R_EDI, w) + w);
       break;
     default:
@@ -714,6 +718,7 @@ static inline void rep(Decode *s, int w) {
       INV(s->pc);
       break;
   }
+  Rw(R_ECX, w, Rr(R_ECX, w) - 1);
   if (Rr(R_ECX, w) > 0) {
     s->dnpc = s->snpc - 2;
   } else {
