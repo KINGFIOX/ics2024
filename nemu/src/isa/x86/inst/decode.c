77231907e4a7bfe65e3a484ffaaba01b5a63abaf
tracer-ics2024 <tracer@njuics.org>
2024-11-15 10:40:21 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.6-2-default #1 SMP PREEMPT_DYNAMIC Mon Nov  4 08:15:48 UTC 2024 (8545f24) x86_64 x86_64 x86_64 GNU/Linux  10:40:21  up  23:38,  7 users,  load average: 1.37, 0.70, 0.45

--- nemu/src/isa/x86/inst/decode.c
+++ nemu/src/isa/x86/inst/decode.c
@@ -695,7 +695,7 @@ static inline void out_(int w, word_t data, word_t port) {
 }
 
 static inline word_t in_(int w, word_t port) {
-  printf("in_: addr = %x\n", port);
+  // printf("in_: addr = %x\n", port);
   uint32_t pio_read(ioaddr_t addr, int len);
   return pio_read(port, w);
 }
