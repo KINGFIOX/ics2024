664349fbeecba673aac0c54aca121a654b312a72
tracer-ics2024 <tracer@njuics.org>
2024-11-19 11:41:35 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.6-2-default #1 SMP PREEMPT_DYNAMIC Mon Nov  4 08:15:48 UTC 2024 (8545f24) x86_64 x86_64 x86_64 GNU/Linux  11:41:35  up 3 days 16:50,  6 users,  load average: 0.24, 0.20, 0.32

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,8 +4,8 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  int w = 800;  // TODO: get the correct width
-  int h = 600;  // TODO: get the correct height
+  AM_GPU_CONFIG_T cfg = io_read(AM_GPU_CONFIG);
+  int w = cfg.width, h = cfg.height;
   uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
   for (int i = 0; i < w * h; i++) fb[i] = i;
   outl(SYNC_ADDR, 1);
@@ -14,7 +14,7 @@ void __am_gpu_init() {
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t screen_wh = inl(VGACTL_ADDR);
   uint32_t h = screen_wh & 0xffff;
-  uint32_t w = screen_wh >> 16;
+  uint32_t w = (screen_wh >> 16) & 0xffff;
   *cfg = (AM_GPU_CONFIG_T){.present = true, .has_accel = false, .width = w, .height = h, .vmemsz = 0};
 }
 
