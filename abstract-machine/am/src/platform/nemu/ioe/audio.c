fb1be3392358971ec69276af9aad619e02b9f9c1
tracer-ics2024 <tracer@njuics.org>
2024-11-19 13:55:34 +0800

    >  compile NEMU 231220000 张三 Linux localhost.localdomain 6.11.6-2-default #1 SMP PREEMPT_DYNAMIC Mon Nov  4 08:15:48 UTC 2024 (8545f24) x86_64 x86_64 x86_64 GNU/Linux  13:55:34  up 3 days 19:04,  6 users,  load average: 1.15, 1.30, 1.21

--- abstract-machine/am/src/platform/nemu/ioe/audio.c
+++ abstract-machine/am/src/platform/nemu/ioe/audio.c
@@ -2,21 +2,30 @@
 #include <klib.h>
 #include <nemu.h>
 
-#define AUDIO_FREQ_ADDR (AUDIO_ADDR + 0x00)       // 0x0000_0200
-#define AUDIO_CHANNELS_ADDR (AUDIO_ADDR + 0x04)   // 0x0000_0204
-#define AUDIO_SAMPLES_ADDR (AUDIO_ADDR + 0x08)    // 0x0000_0207
+#include "klib-macros.h"
+
+static int buf_pos = 0;
+
+void __am_audio_init() {
+  AM_AUDIO_CONFIG_T cfg __attribute__((unused)) = io_read(AM_AUDIO_CONFIG);
+  // do nothing
+}
+
+/* ---------- audio config ---------- */
+
 #define AUDIO_SBUF_SIZE_ADDR (AUDIO_ADDR + 0x0c)  // 0x0000_020c
-#define AUDIO_INIT_ADDR (AUDIO_ADDR + 0x10)       // 0x0000_0210
-#define AUDIO_COUNT_ADDR (AUDIO_ADDR + 0x14)      // 0x0000_0214
-
-__attribute__((unused)) static int sbuf_pos = 0;
-
-void __am_audio_init() {}
 
 void __am_audio_config(AM_AUDIO_CONFIG_T *cfg) {
   int bufsize = inl(AUDIO_SBUF_SIZE_ADDR);
-  (*cfg) = (AM_AUDIO_CONFIG_T){.present = true, .bufsize = bufsize};
+  (*cfg) = (AM_AUDIO_CONFIG_T){.present = false, .bufsize = bufsize};
 }
+
+/* ---------- audio ctrl ---------- */
+
+#define AUDIO_FREQ_ADDR (AUDIO_ADDR + 0x00)      // 0x0000_0200
+#define AUDIO_CHANNELS_ADDR (AUDIO_ADDR + 0x04)  // 0x0000_0204
+#define AUDIO_SAMPLES_ADDR (AUDIO_ADDR + 0x08)   // 0x0000_0207
+#define AUDIO_INIT_ADDR (AUDIO_ADDR + 0x10)      // 0x0000_0210
 
 void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   outl(AUDIO_FREQ_ADDR, ctrl->freq);
@@ -25,23 +34,28 @@ void __am_audio_ctrl(AM_AUDIO_CTRL_T *ctrl) {
   outl(AUDIO_INIT_ADDR, 1);
 }
 
-void __am_audio_status(AM_AUDIO_STATUS_T *stat) { stat->count = inl(AUDIO_COUNT_ADDR); }
+/* ---------- audio ctrl ---------- */
+
+#define AUDIO_COUNT_ADDR (AUDIO_ADDR + 0x14)  // 0x0000_0214
+
+void __am_audio_status(AM_AUDIO_STATUS_T *stat) {
+  stat->count = inl(AUDIO_COUNT_ADDR);
+  //
+}
 
 void __am_audio_play(AM_AUDIO_PLAY_T *ctl) {
-  // TODO:
-  // uint8_t *audio_data = (ctl->buf).start;
-  // uint32_t sbuf_size = inl(AUDIO_SBUF_SIZE_ADDR);
-  // uint32_t cnt = inl(AUDIO_COUNT_ADDR);
-  // uint32_t len = ((ctl->buf).end - (ctl->buf).start);
+  const uint32_t buf_size = inl(AUDIO_SBUF_SIZE_ADDR) / sizeof(uint8_t);
 
-  // while (len > sbuf_size - cnt) {
-  //   ;
-  // }
+  uint8_t *audio = (ctl->buf).start;
+  uint32_t len = ((ctl->buf).end - (ctl->buf).start) / sizeof(uint8_t);
 
-  // uint8_t *stream_buf = (uint8_t *)(uintptr_t)AUDIO_SBUF_ADDR;
-  // for (int i = 0; i < len; i++) {
-  //   stream_buf[sbuf_pos] = audio_data[i];
-  //   sbuf_pos = (sbuf_pos + 1) % sbuf_size;
-  // }
-  // outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len);
+  while (len > buf_size - inl(AUDIO_COUNT_ADDR));  // blocking
+
+  uint8_t *ab = (uint8_t *)(uintptr_t)AUDIO_ADDR;
+  for (int i = 0; i < len; i++) {
+    ab[buf_pos] = audio[i];
+    buf_pos = (buf_pos + 1) % buf_size;
+  }
+
+  outl(AUDIO_COUNT_ADDR, inl(AUDIO_COUNT_ADDR) + len);
 }
